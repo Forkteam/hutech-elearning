987ac926e2279ae2f16d17a19710dab36b3cdf7f
nhuthuynhphu <nhuthuynhphu@github.com>
2021-12-17 22:10:13 +0700

    change copyright landing

--- client/src/components/layout/landing.js
+++ client/src/components/layout/landing.js
@@ -1,38 +1,20 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
 import CameraIcon from '@mui/icons-material/PhotoCamera';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { IconButton, StepIcon } from '@mui/material';
-import { red } from '@mui/material/colors';
-import { border, color, height, sizeHeight, sizeWidth, sizing, width } from '@mui/system';
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright ©'}
-      <Link
-        sx={{ color: 'red'}} 
-        color="inherit" href="https://www.facebook.com/CrisAn.2001">
-          Nhóm 2 - 19DTHD4
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import * as React from 'react';
+import Copyright from './copyright';
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
@@ -44,18 +26,20 @@ export default function Album() {
       <CssBaseline />
       <AppBar position="relative">
         <Toolbar position="relative">
-          <CameraIcon sx={{ mr: 4  }} />
+          <CameraIcon sx={{ mr: 4 }} />
           <Typography variant="h5" color="white" fontWeight="500" noWrap>
-            HUTECH - Đại Học Công Nghệ TP.HCM 
+            HUTECH - Đại Học Công Nghệ TP.HCM
           </Typography>
-          <Button sx={{ 
-            background: 'rgba(209, 228, 184, 0.79)',            
-            color: 'black',
-            border: 2,
-            right: 10,
-            top: 12,
-            position: 'absolute'
-            }} >
+          <Button
+            sx={{
+              background: 'rgba(209, 228, 184, 0.79)',
+              color: 'black',
+              border: 2,
+              right: 10,
+              top: 12,
+              position: 'absolute',
+            }}
+          >
             Đăng Nhập
           </Button>
         </Toolbar>
@@ -67,7 +51,8 @@ export default function Album() {
             pt: 8,
             pb: 6,
             color: 'red',
-            background: 'url(https://file1.hutech.edu.vn/file/editor/homepage1/Khu%20E%20%288%29.jpg) no-repeat center/ cover',
+            background:
+              'url(https://file1.hutech.edu.vn/file/editor/homepage1/Khu%20E%20%288%29.jpg) no-repeat center/ cover',
           }}
         >
           <Container maxWidth="sm">
@@ -76,13 +61,19 @@ export default function Album() {
               variant="h2"
               align="center"
               color="darkblue"
-              fontWeight='400'
+              fontWeight="400"
               gutterBottom
             >
               Góc Học Tập
             </Typography>
-            <Typography fontWeight="500" variant="h5" align="center" color="black" paragraph>
-              Môi trường chia sẻ tài liệu học tập cho sinh viên, giúp nâng cao 
+            <Typography
+              fontWeight="500"
+              variant="h5"
+              align="center"
+              color="black"
+              paragraph
+            >
+              Môi trường chia sẻ tài liệu học tập cho sinh viên, giúp nâng cao
               kiến thức chuyên môn, đạt thành tích tốt trong học tập.
             </Typography>
             <Stack
@@ -103,24 +94,28 @@ export default function Album() {
             {cards.map((card) => (
               <Grid item key={card} xs={12} sm={6} md={4}>
                 <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column'}}
+                  sx={{
+                    height: '100%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                  }}
                 >
-                  <CardMedia 
+                  <CardMedia
                     component="img"
                     sx={{
                       // 16:9
                       pt: '10px',
                     }}
                     image="https://source.unsplash.com/random"
-                    alt="random"                  
+                    alt="random"
                   />
-                  <CardContent  sx={{ flexGrow: 1 }}>
+                  <CardContent sx={{ flexGrow: 1 }}>
                     <Typography gutterBottom variant="h5" component="h2">
                       Heading
                     </Typography>
                     <Typography>
-                      This is a media card. You can use this section to describe the
-                      content.
+                      This is a media card. You can use this section to describe
+                      the content.
                     </Typography>
                   </CardContent>
                   <CardActions>
