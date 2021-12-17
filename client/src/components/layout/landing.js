988449401352b66e78cffc5869ea0621356cf8b9
unknown <duongquocan222@gmail.com>
2021-12-17 10:17:14 +0700

    homepage

--- client/src/components/layout/landing.js
+++ client/src/components/layout/landing.js
@@ -1,7 +1,153 @@
-import { Redirect } from 'react-router-dom';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { IconButton, StepIcon } from '@mui/material';
+import { red } from '@mui/material/colors';
+import { border, color, height, sizeHeight, sizeWidth, sizing, width } from '@mui/system';
 
-const Landing = () => {
-  return <Redirect to="/login" />;
-};
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link
+        sx={{ color: 'red'}} 
+        color="inherit" href="https://www.facebook.com/CrisAn.2001">
+        Nhóm 2 - 19DTHD4
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-export default Landing;
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const theme = createTheme();
+
+export default function Album() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar position="relative">
+          <CameraIcon sx={{ mr: 4  }} />
+          <Typography variant="h5" color="white" fontWeight="500" noWrap>
+            HUTECH - Đại Học Công Nghệ TP.HCM 
+          </Typography>
+          <Button sx={{ 
+            background: 'rgba(209, 228, 184, 0.79)',            
+            color: 'black',
+            border: 2,
+            right: 10,
+            top: 12,
+            position: 'absolute'
+            }} >
+            Đăng Nhập
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            pt: 8,
+            pb: 6,
+            color: 'red',
+            background: 'url(https://file1.hutech.edu.vn/file/editor/homepage1/Khu%20E%20%288%29.jpg) no-repeat center/ cover',
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="darkblue"
+              fontWeight='400'
+              
+              gutterBottom
+            >
+              Góc Học Tập
+            </Typography>
+            <Typography fontWeight="500" variant="h5" align="center" color="black" paragraph>
+              Môi trường chia sẻ tài liệu học tập cho sinh viên, giúp nâng cao 
+              kiến thức chuyên môn, đạt thành tích tốt trong học tập.
+            </Typography>
+            <Stack
+              sx={{ pt: 4 }}
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+              <Button variant="contained">Tin Tức</Button>
+              <Button variant="outlined">Secondary action</Button>
+            </Stack>
+          </Container>
+        </Box>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      pt: '56.25%',
+                    }}
+                    image="https://source.unsplash.com/random"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the
+                      content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Xem chi tiết</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+          HUTECH
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          Môi trường học tập tiến bộ.
+        </Typography>
+        <Copyright />
+      </Box>
+      {/* End footer */}
+    </ThemeProvider>
+  );
+}
