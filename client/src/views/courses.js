0d1e895c9ddea5c797d60510bd57522433ad2ae2
nhuthuynhphu <nhuthuynhphu@github.com>
2021-12-18 22:19:49 +0700

    courses

--- client/src/views/courses.js
+++ client/src/views/courses.js
@@ -7,47 +7,142 @@ import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
 const Courses = () => {
   return (
     <Container sx={{ py: 4 }} maxWidth="md">
       {/* End hero unit */}
       <Grid container spacing={4}>
-        {cards.map((card) => (
-          <Grid item key={card} xs={12} sm={6} md={4}>
-            <Card
+        <Grid item xs={12} sm={6} md={4}>
+          <Card
+            sx={{
+              height: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              boxShadow: 3,
+            }}
+          >
+            <CardMedia
+              component="img"
               sx={{
-                height: '100%',
-                display: 'flex',
-                flexDirection: 'column',
-                boxShadow: 3,
+                // 16:9
+                pt: '10px',
               }}
-            >
-              <CardMedia
-                component="img"
-                sx={{
-                  // 16:9
-                  pt: '10px',
-                }}
-                image="https://source.unsplash.com/random"
-                alt="random"
-              />
-              <CardContent sx={{ flexGrow: 1 }}>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Heading
-                </Typography>
-                <Typography>
-                  This is a media card. You can use this section to describe the
-                  content.
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Xem chi tiết</Button>
-              </CardActions>
-            </Card>
-          </Grid>
-        ))}
+              image="https://source.unsplash.com/random"
+              alt="random"
+            />
+            <CardContent sx={{ flexGrow: 1 }}>
+              <Typography gutterBottom variant="h5" component="h2">
+                Heading
+              </Typography>
+              <Typography>
+                This is a media card. You can use this section to describe the
+                content.
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Xem chi tiết</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={4}>
+          <Card
+            sx={{
+              height: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              boxShadow: 3,
+            }}
+          >
+            <CardMedia
+              component="img"
+              sx={{
+                // 16:9
+                pt: '10px',
+              }}
+              image="https://source.unsplash.com/random"
+              alt="random"
+            />
+            <CardContent sx={{ flexGrow: 1 }}>
+              <Typography gutterBottom variant="h5" component="h2">
+                Heading
+              </Typography>
+              <Typography>
+                This is a media card. You can use this section to describe the
+                content.
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Xem chi tiết</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={4}>
+          <Card
+            sx={{
+              height: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              boxShadow: 3,
+            }}
+          >
+            <CardMedia
+              component="img"
+              sx={{
+                // 16:9
+                pt: '10px',
+              }}
+              image="https://source.unsplash.com/random"
+              alt="random"
+            />
+            <CardContent sx={{ flexGrow: 1 }}>
+              <Typography gutterBottom variant="h5" component="h2">
+                Heading
+              </Typography>
+              <Typography>
+                This is a media card. You can use this section to describe the
+                content.
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Xem chi tiết</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={6} md={4}>
+          <Card
+            sx={{
+              height: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              boxShadow: 3,
+            }}
+          >
+            <CardMedia
+              component="img"
+              sx={{
+                // 16:9
+                pt: '10px',
+              }}
+              image="https://source.unsplash.com/random"
+              alt="random"
+            />
+            <CardContent sx={{ flexGrow: 1 }}>
+              <Typography gutterBottom variant="h5" component="h2">
+                Heading
+              </Typography>
+              <Typography>
+                This is a media card. You can use this section to describe the
+                content.
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Xem chi tiết</Button>
+            </CardActions>
+          </Card>
+        </Grid>
       </Grid>
     </Container>
   );
