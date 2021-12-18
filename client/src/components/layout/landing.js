7aa9fc79bcf2709a6e50da05e7f2c94ed09f9658
unknown <duongquocan222@gmail.com>
2021-12-18 23:01:38 +0700

    homepage-2

--- client/src/components/layout/landing.js
+++ client/src/components/layout/landing.js
@@ -14,7 +14,6 @@ import Typography from '@mui/material/Typography';
 import LogoHutech from '../../assets/logo.png';
 import Copyright from './copyright';
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const theme = createTheme();
 
@@ -34,7 +33,7 @@ export default function Album() {
         >
           Đại Học Công Nghệ TP.HCM - HUTECH
         </Typography>
-        <Button variant="outlined" size="medium">
+        <Button variant="contained" size="medium">
           Đăng Nhập
         </Button>
       </Toolbar>
@@ -102,26 +101,182 @@ export default function Album() {
         <Container sx={{ py: 8 }} maxWidth="md">
           {/* End hero unit */}
           <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
+
+              <Grid  xs={12} sm={6} md={4}>
                 <Card
                   sx={{
-                    height: '100%',
+                    height: '100%',                  
                     display: 'flex',
                     flexDirection: 'column',
+                    m: '10px',
                   }}
                 >
                   <CardMedia
                     component="img"
                     sx={{
                       // 16:9
-                      pt: '10px',
+                      m: '15px',
+                    }}
+                    image="https://vnypc.files.wordpress.com/2016/11/html_css.jpg?w=300&h=200"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2" color='darkblue' align='center'>
+                      HTML/CSS
+                    </Typography>
+                    <Typography>
+                      HTML (hay Hypertext Markup Language) là ngôn ngữ đánh dấu siêu văn bản, CSS (hay Cascading Style Sheet language) được định nghĩa là ngôn ngữ tạo phong cách cho trang web.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Xem chi tiết</Button>
+                  </CardActions>
+                </Card>
+              </Grid>    
+              <Grid  xs={12} sm={6} md={4}>
+                <Card
+                  sx={{
+                    height: '100%',                  
+                    display: 'flex',
+                    flexDirection: 'column',
+                    m: '10px',
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      m: '15px',
+                    }}
+                    image="https://thienanblog.com/wp-content/uploads/2015/04/javascript_logo.png"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2" color='darkblue' align='center'>
+                      Javascript
+                    </Typography>
+                    <Typography>
+                      JavaScript, theo phiên bản hiện hành, là một ngôn ngữ lập trình thông dịch được phát triển từ các ý niệm nguyên mẫu. Ngôn ngữ này được dùng rộng rãi cho các trang web cũng như phía máy chủ.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Xem chi tiết</Button>
+                  </CardActions>
+                </Card>
+              </Grid>    
+              <Grid  xs={12} sm={6} md={4}>
+                <Card
+                  sx={{
+                    height: '100%',                  
+                    display: 'flex',
+                    flexDirection: 'column',
+                    m: '10px',
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      m: '15px',
+                    }}
+                    image="https://zigexn-ventura.github.io/assets/images/react-logo.png"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2" color='darkblue' align='center'>
+                      React JS
+                    </Typography>
+                    <Typography>
+                      Được dịch từ tiếng Anh-React là một thư viện JavaScript front-end mã nguồn mở miễn phí để xây dựng giao diện người dùng dựa trên các thành phần UI. Nó được duy trì bởi Meta và một cộng đồng các nhà phát triển và công ty cá nhân. React có thể được sử dụng như một cơ sở để phát triển các ứng dụng trang đơn hoặc di động.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Xem chi tiết</Button>
+                  </CardActions>
+                </Card>
+              </Grid>    
+              <Grid  xs={12} sm={6} md={4}>
+                <Card
+                  sx={{
+                    height: '100%',                  
+                    display: 'flex',
+                    flexDirection: 'column',
+                    m: '10px',
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      m: '15px',
+                    }}
+                    image="https://bachasoftware.com/wp-content/uploads/elementor/thumbs/nodejslogo-p3zvdhaajh0bxurlgqp1gszveuzuf58gd4auf7uve8.png"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2" color='darkblue' align='center'>
+                      Node JS
+                    </Typography>
+                    <Typography>
+                      Node.js là một hệ thống phần mềm được thiết kế để viết các ứng dụng internet có khả năng mở rộng, đặc biệt là máy chủ web. Chương trình được viết bằng JavaScript, sử dụng kỹ thuật điều khiển theo sự kiện, nhập/xuất không đồng bộ để tối thiểu tổng chi phí và tối đa khả năng mở rộng. 
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Xem chi tiết</Button>
+                  </CardActions>
+                </Card>
+              </Grid>    
+              <Grid  xs={12} sm={6} md={4}>
+                <Card
+                  sx={{
+                    height: '100%',                  
+                    display: 'flex',
+                    flexDirection: 'column',
+                    m: '10px',
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      m: '15px',
+                    }}
+                    image="https://laptrinhcanban.com/css/images/page/java-co-ban-cho-nguoi-moi-bat-dau.webp?ezimgfmt=rs%3Adevice%2Frscb2-1"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2" color='darkblue' align='center'>
+                      Java
+                    </Typography>
+                    <Typography>
+                      Java là một ngôn ngữ lập trình hướng đối tượng, dựa trên lớp được thiết kế để có càng ít phụ thuộc thực thi càng tốt.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">Xem chi tiết</Button>
+                  </CardActions>
+                </Card>
+              </Grid>    
+              <Grid  xs={12} sm={6} md={4}>
+                <Card
+                  sx={{
+                    height: '100%',                  
+                    display: 'flex',
+                    flexDirection: 'column',
+                    m: '10px',
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      m: '15px',
                     }}
                     image="https://source.unsplash.com/random"
                     alt="random"
                   />
                   <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
+                    <Typography gutterBottom variant="h5" component="h2" color='darkblue' align='center'>
                       Heading
                     </Typography>
                     <Typography>
@@ -133,8 +288,9 @@ export default function Album() {
                     <Button size="small">Xem chi tiết</Button>
                   </CardActions>
                 </Card>
-              </Grid>
-            ))}
+              </Grid>    
+              
+              
           </Grid>
         </Container>
       </main>
