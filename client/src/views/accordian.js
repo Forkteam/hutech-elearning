d15b8b977c6291ef52f7e4e2eeacacdfa247b6a6
= <lehuynhphuongtung02@gmail.com>
2021-12-22 02:10:33 +0700

    accordian

--- client/src/views/accordian.js
+++ client/src/views/accordian.js
@@ -7,6 +7,7 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { styled } from '@mui/material/styles';
 import MuiGrid from '@mui/material/Grid';
 import CheckIcon from '@mui/icons-material/Check';
+
 const Grid = styled(MuiGrid)(({ theme }) => ({
   width: '100%',
   ...theme.typography.body2,
@@ -18,30 +19,30 @@ const Grid = styled(MuiGrid)(({ theme }) => ({
 export default function SimpleAccordion() {
   return (
     <div>
-    <Typography variant="h1" >
+    <Typography variant="h1" marginBottom="25px">
     JavaScript Cơ Bản
     </Typography>
     <Typography >
-    Học Javascript cơ bản phù hợp cho người chưa từng học lập trình. Với hơn 100 bài học và có bài tập thực hành sau mỗi bài học.
+    Học Javascript cơ bản phù hợp cho người chưa từng học lập trình. Với những bài học và bài tập thực hành hay sau mỗi bài học.
     </Typography>
-    <Typography variant="h3" >
+    <Typography variant="h3" marginTop="25px" marginBottom="25px">
     Bạn sẽ học được gì
     </Typography>
     <Grid container>
       <Grid item xs>
-        <Typography><CheckIcon/>Hiểu chi tiết về các khái niệm cơ bản trong JS</Typography> 
-        <Typography><CheckIcon/>Tự tin khi phỏng vấn với kiến thức vững chắc</Typography>
-        <Typography><CheckIcon/>Nắm chắc các tính năng trong phiên bản ES6</Typography> 
-        <Typography><CheckIcon/>Ghi nhớ các khái niệm nhờ bài tập trắc nghiệm</Typography>
+        <Typography><CheckIcon color="secondary" />Hiểu chi tiết về các khái niệm cơ bản trong JS</Typography> 
+        <Typography><CheckIcon color="secondary"/>Tự tin khi phỏng vấn với kiến thức vững chắc</Typography>
+        <Typography><CheckIcon color="secondary"/>Nắm chắc các tính năng trong phiên bản ES6</Typography> 
+        <Typography><CheckIcon color="secondary"/>Ghi nhớ các khái niệm nhờ bài tập trắc nghiệm</Typography>
       </Grid>
       <Grid item xs>
-      <Typography><CheckIcon/>Xây dựng được website đầu tiên kết hợp với JS</Typography> 
-      <Typography><CheckIcon/>Có nền tảng để học các thư viện và framework JS</Typography> 
-      <Typography><CheckIcon/>Thành thạo DOM APIs để tương tác với trang web</Typography> 
-      <Typography><CheckIcon/>Nâng cao tư duy với các bài kiểm tra với testcases</Typography> 
+      <Typography><CheckIcon color="secondary"/>Xây dựng được website đầu tiên kết hợp với JS</Typography> 
+      <Typography><CheckIcon color="secondary"/>Có nền tảng để học các thư viện và framework JS</Typography> 
+      <Typography><CheckIcon color="secondary"/>Thành thạo DOM APIs để tương tác với trang web</Typography> 
+      <Typography><CheckIcon color="secondary"/>Nâng cao tư duy với các bài kiểm tra với testcases</Typography> 
       </Grid>
     </Grid>
-    <Typography variant="h3" >
+    <Typography variant="h3" marginTop="25px" marginBottom="25px">
     Nội dung khóa học
     </Typography>
 
@@ -51,13 +52,20 @@ export default function SimpleAccordion() {
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography>Accordion 1</Typography>
+          <Typography>1. Giới thiệu</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+      
+        <Accordion>
+        <AccordionSummary>
+          <Typography>1.1 Giới thiệu tổng quan</Typography>
+        </AccordionSummary>
+        <AccordionSummary>
+          <Typography>1.2 Cài đặt</Typography>
+        </AccordionSummary>
+      </Accordion>
+
+
         </AccordionDetails>
       </Accordion>
       <Accordion>
@@ -66,13 +74,19 @@ export default function SimpleAccordion() {
           aria-controls="panel2a-content"
           id="panel2a-header"
         >
-          <Typography>Accordion 2</Typography>
+          <Typography>2. Làm quen</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+          
+        <Accordion>
+        <AccordionSummary>
+          <Typography>2.1 Giới thiệu tổng quan</Typography>
+        </AccordionSummary>
+        <AccordionSummary>
+          <Typography>2.2 Cài đặt</Typography>
+        </AccordionSummary>
+        </Accordion>
+
         </AccordionDetails>
       </Accordion>
       <Accordion>
@@ -81,12 +95,12 @@ export default function SimpleAccordion() {
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography>Accordion 3</Typography>
+          <Typography>3. Kiến thức cốt lỗi</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-          s
-          </Typography>
+          
+
+        
         </AccordionDetails>
       </Accordion>
 
@@ -96,12 +110,12 @@ export default function SimpleAccordion() {
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography>Accordion 4</Typography>
+          <Typography>4. HTML DOM</Typography>
         </AccordionSummary>
         <AccordionDetails> 
-        <Typography>
-          s
-        </Typography>
+          
+
+
         </AccordionDetails>
       </Accordion>
       <Accordion>
@@ -110,26 +124,42 @@ export default function SimpleAccordion() {
           aria-controls="panel1a-content"
           id="panel1a-header"
         >
-          <Typography>Accordion 5</Typography>
+          
+          <Typography>5. ECMAScript 6+</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
+          
+
+
         </AccordionDetails>
       </Accordion>
-     
-      <Typography variant="h3" >
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          
+          <Typography>6. Thực hành vận dụng</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          
+
+
+        </AccordionDetails>
+      </Accordion>
+      
+
+      <Typography variant="h3" marginTop="25px" marginBottom="25px">
       Yêu cầu
       </Typography>
       <Grid container>
         <Grid item xs>
-          <Typography><CheckIcon/>Máy vi tính kết nối internet (Windows, Ubuntu hoặc MacOS)</Typography> 
-          <Typography><CheckIcon/>Ý thức tự học cao, trách nhiệm cao, kiên trì bền bỉ không ngại cái khó</Typography>
-          <Typography><CheckIcon/>Không được nóng vội, bình tĩnh học, làm bài tập sau mỗi bài học</Typography> 
-          <Typography><CheckIcon/>Khi học nếu có khúc mắc hãy tham gia hỏi/đáp tại group FB: Học lập trình web (fullstack.edu.vn)</Typography>
-          <Typography><CheckIcon/>Bạn không cần biết gì hơn nữa, trong khóa học tôi sẽ chỉ cho bạn những gì bạn cần phải biết</Typography>
+          <Typography><CheckIcon color="secondary"/>Máy vi tính kết nối internet (Windows, Ubuntu hoặc MacOS)</Typography> 
+          <Typography><CheckIcon color="secondary"/>Ý thức tự học cao, trách nhiệm cao, kiên trì bền bỉ không ngại cái khó</Typography>
+          <Typography><CheckIcon color="secondary"/>Không được nóng vội, bình tĩnh học, làm bài tập sau mỗi bài học</Typography> 
+          <Typography><CheckIcon color="secondary"/>Khi học nếu có khúc mắc hãy tham gia hỏi/đáp tại group FB: Học lập trình web (fullstack.edu.vn)</Typography>
+          <Typography marginBottom="100px"><CheckIcon color="secondary"/>Bạn không cần biết gì hơn nữa, trong khóa học tôi sẽ chỉ cho bạn những gì bạn cần phải biết</Typography>
         </Grid>
       </Grid>
     </div>
