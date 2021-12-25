f4a62ce65c60b56ab9ff8ea22ae76971284e1c0d
nhuthuynhphu <nhuthuynhphu@github.com>
2021-12-25 22:52:03 +0700

    reset password

--- client/src/components/auth/reset-password.js
+++ client/src/components/auth/reset-password.js
@@ -1,40 +1,91 @@
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
-import { Link } from 'react-router-dom';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import queryString from 'query-string';
+import { useContext, useState } from 'react';
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import { AuthContext } from '../../contexts/auth-context';
+import AlertMessage from '../layouts/alert-message';
 import Copyright from '../layouts/copyright';
 
 const ResetPassword = () => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    // eslint-disable-next-line no-console
-    console.log({
-      email: data.get('email'),
+  const history = useHistory();
+  const location = useLocation();
+  const { token, id } = queryString.parse(location.search);
+  const { sendMailResetPassword, resetPassword } = useContext(AuthContext);
+  const [alert, setAlert] = useState(null);
+  const [email, setEmail] = useState('');
+  const [resetPasswordForm, setResetPasswordForm] = useState({
+    token,
+    id,
+    password: '',
+    confirmPassword: '',
+  });
+  const { password, confirmPassword } = resetPasswordForm;
+
+  const onChangePasswordForm = (event) =>
+    setResetPasswordForm({
+      ...resetPasswordForm,
+      [event.target.name]: event.target.value,
     });
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let registerData;
+      if (token === undefined || id === undefined) {
+        registerData = await sendMailResetPassword(email);
+        setAlert({ type: 'warning', message: 'Please wait' });
+        setTimeout(() => setAlert(null), 1000);
+      } else {
+        if (password.length < 6) {
+          setAlert({
+            type: 'error',
+            message: "Password mustn't be less than 6 characters",
+          });
+          setTimeout(() => setAlert(null), 5000);
+          return;
+        }
+        if (password !== confirmPassword) {
+          setAlert({ type: 'error', message: 'Password does not match' });
+          setTimeout(() => setAlert(null), 5000);
+          return;
+        }
+        registerData = await resetPassword(resetPasswordForm);
+        setAlert({ type: 'warning', message: 'Please wait' });
+        setTimeout(() => setAlert(null), 1000);
+      }
+      if (!registerData.success) {
+        setAlert({ type: 'error', message: registerData.message });
+        setTimeout(() => setAlert(null), 3000);
+      } else {
+        setAlert({ type: 'success', message: registerData.message });
+        setTimeout(() => {
+          setAlert(null);
+          history.push('/login');
+        }, 2000);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  return (
-    <>
-      <Typography component="h1" variant="h5">
-        Quên mật khẩu
-      </Typography>
-      <Box
-        component="form"
-        onSubmit={handleSubmit}
-        sx={{ mt: 1, width: '-webkit-fill-available' }}
-      >
+  let body;
+  if (token === undefined || id === undefined) {
+    body = (
+      <>
         <TextField
           margin="normal"
           required
           fullWidth
-          id="email"
           label="Email của bạn"
           name="email"
           autoComplete="email"
           autoFocus
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
         />
         <Button
           type="submit"
@@ -44,6 +95,57 @@ const ResetPassword = () => {
         >
           Gửi liên kết đến email
         </Button>
+      </>
+    );
+  } else {
+    body = (
+      <>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Mật khẩu"
+          type="password"
+          autoComplete="current-password"
+          onChange={onChangePasswordForm}
+          value={password}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="confirmPassword"
+          label="Nhập lại mật khẩu"
+          type="password"
+          autoComplete="current-confirmPassword"
+          onChange={onChangePasswordForm}
+          value={confirmPassword}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Đổi mật khẩu
+        </Button>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Typography component="h1" variant="h5">
+        Quên mật khẩu
+      </Typography>
+      <Box
+        component="form"
+        onSubmit={onSubmit}
+        sx={{ mt: 1, width: '-webkit-fill-available' }}
+      >
+        <AlertMessage info={alert} />
+        {body}
         <Grid container>
           <Grid item>
             <Link to="/login" variant="body2">
