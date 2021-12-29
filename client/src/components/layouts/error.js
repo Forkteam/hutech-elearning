55246ccd6025233bc62011f339059d7515e3bc8e
unknown <duongquocan222@gmail.com>
2021-12-29 22:40:51 +0700

    update 404 error page

--- client/src/components/layouts/error.js
+++ client/src/components/layouts/error.js
@@ -1,7 +1,7 @@
 // import { useHistory } from 'react-router-dom';
 //import { Card, Button } from 'react-bootstrap';
 import { Button, Typography } from "@mui/material";
-import { Box } from "@mui/system";
+import { Box, fontSize } from "@mui/system";
 import { Link } from "react-router-dom";
 
 const Error = () => {
@@ -12,6 +12,7 @@ const Error = () => {
       height:'100vh',
       background:'url(https://scr.vn/wp-content/uploads/2020/07/%E1%BA%A2nh-Galaxy-lung-linh-huy%E1%BB%81n-%E1%BA%A3o.jpg) no-repeat center / cover',
       display:'flex',
+      fontSize:'100%',
       }}>
       <Box height='60%' width='50%' 
         sx={{
@@ -21,20 +22,33 @@ const Error = () => {
           borderRadius:20,
         }}>
        <Typography align='center'>
-         <Typography fontSize="4vw" fontWeight='600' color='rgba(255, 0, 0, 0.8)'>
+         <Typography marginTop="6vh" fontSize="4vw" fontWeight='600' color='rgba(255, 0, 0, 0.8)'>
             404 : Error Page
          </Typography>
          <Typography fontSize="2.17vw" fontWeight='500' color='rgba(247, 242, 242, 0.7)'>
            Không tìm thấy trang bạn muốn truy cập.
          </Typography>
          <Typography height="1vh" fontSize="1.8vw" fontWeight='500' color='rgba(247, 242, 242, 0.7)'>
-           <br/>Vui lòng thử lại hoặc liên hệ <Button sx={{ display:'flex', fontSize:'1.5vw', fontWeight:'700' ,color:'rgba(220, 157, 30, 0.85)' }} href="https://youtu.be/PaBkXQhxwto">Support@TeamDoAn</Button>
+           <br/>Vui lòng thử lại hoặc liên hệ <Button sx={{ letterSpacing:4 ,display:'flex', fontSize:'1.5vw', fontWeight:'700' ,color:'rgba(220, 157, 30, 0.85)' }} href="https://youtu.be/PaBkXQhxwto">Support@TeamDoAn</Button>
          </Typography>
        </Typography>
 
        <Box>
        <Link to="/">
-            <Button sx={{ background:'rgba(6, 79, 224, 0.67)' ,borderRadius:'20px', fontSize:'80%%', height:40, width:180 , position:'absolute', bottom: "15%" , left:'38%', right:'38%' }} variant="contained">Quay lại Trang Chủ</Button>
+            <Button sx={{ 
+              background:'rgba(6, 79, 224, 0.67)',
+              borderRadius:'20px', 
+              height:"11%",
+              width:"30%", 
+              position:'absolute', 
+              top:"78%",
+              bottom: "15%", 
+              left:'35%', 
+              right:'35%',
+              fontSize:'1.3vw',
+              }} variant="contained">
+              Quay lại trang chủ
+              </Button>
           </Link>
        </Box>
 
