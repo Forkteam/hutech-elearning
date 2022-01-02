bac7881015cf6cc96690656f976cf77fd754ebf9
nhuthuynhphu <nhuthuynhphu@github.com>
2022-01-02 09:27:21 +0700

    personal data

--- client/src/views/personal.js
+++ client/src/views/personal.js
@@ -1,28 +1,39 @@
 import { TextField, Typography, Grid } from '@mui/material';
 import { Box } from '@mui/system';
-import * as React from 'react';
+import moment from 'moment';
+import 'moment/locale/vi';
+import { useContext } from 'react';
+import { AuthContext } from '../contexts/auth-context';
+moment.locale('vi');
 
 export default function Personal() {
+  const {
+    authState: { user },
+  } = useContext(AuthContext);
+
   return (
     <div className="personal">
       <Grid container spacing={2}>
-        <Grid item xs={12} sm={6} md={3}>
+        <Grid item xs={12} sm={4} md={3}>
           <div className="div1">
             <Box className="box1">
               {' '}
               <img
                 alt="img"
-                src="https://i1.wp.com/www.polar-pinguin.berlin/wp-content/uploads/2017/12/image_preview.png?w=1080&ssl=1"
+                src={
+                  user?.avatar ||
+                  'https://i1.wp.com/www.polar-pinguin.berlin/wp-content/uploads/2017/12/image_preview.png?w=1080&ssl=1'
+                }
               ></img>
             </Box>
           </div>
           <div className="div2">
             <Box className="box2">
-              Cập nhật gần nhất: 01 giờ 21 phút, ngày 31/12/2021{' '}
+              Cập nhật gần nhất: {moment(user?.updatedAt).fromNow()}{' '}
             </Box>
           </div>
         </Grid>
-        <Grid item xs={12} sm={6} md={9}>
+        <Grid item xs={12} sm={8} md={9}>
           <div className="div4">
             <Typography>
               <div className="row" style={{ marginBottom: '20px' }}>
@@ -30,20 +41,21 @@ export default function Personal() {
                   <h4>
                     <div className="col-md-6" sx={{}}>
                       Mã số sinh viên:{' '}
-                      <span className="font-weight-bold">1911064729</span>
+                      <span className="font-weight-bold">{user?.code}</span>
                     </div>
                     <div className="col-md-6">
                       Họ tên:{' '}
-                      <span className="font-weight-bold">Nguyễn Văn Chuẩn</span>
+                      <span className="font-weight-bold">{user?.fullName}</span>
                     </div>
                     <div className="col-md-6">
-                      Ngày sinh: <span>13-01-2001</span>
+                      Ngày sinh:{' '}
+                      <span>{moment(user?.birthday).format('l')}</span>
                     </div>
                     <div className="col-md-6">
-                      Điện thoại: <span>0865562385</span>
+                      Điện thoại: <span>{user?.phone}</span>
                     </div>
                     <div className="col-md-6">
-                      Email: <span>nguyenvanchuan13012001@gmail.com</span>
+                      Email: <span>{user?.email}</span>
                     </div>
                   </h4>
                 </Box>
