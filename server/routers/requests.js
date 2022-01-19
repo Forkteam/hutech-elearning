b4153809f87f48a99a12ad56a1d64c8150b7c870
nhuthuynhphu <nhuthuynhphu@github.com>
2022-01-19 21:58:04 +0700

    api vnpay

--- server/routers/requests.js
+++ server/routers/requests.js
@@ -2,7 +2,10 @@ import express from 'express';
 import {
   createRequest,
   getRequests,
-  updateRequest
+  updateRequest,
+  createPayment,
+  vnpayReturn
+  // vnpayIpn
 } from '../controllers/requests.js';
 import authTeacher from '../middleware/auth-teacher.js';
 import verifyToken from '../middleware/auth.js';
@@ -12,5 +15,8 @@ const router = express.Router();
 router.get('/', verifyToken, getRequests);
 router.post('/', verifyToken, createRequest);
 router.put('/:id', verifyToken, authTeacher, updateRequest);
+router.post('/create_payment_url', verifyToken, createPayment);
+router.get('/vnpay_return', vnpayReturn);
+// router.get('/vnpay_ipn', vnpayIpn);
 
 export default router;
